85edd58a382d2dcbb29dd32886c1bade8c4d35a9
ElitePat <nsundimbolipatrick@gmail.com>
2026-02-18 06:06:20 +0100

    Ajout methode custom

--- src/sssp_custom.c
+++ src/sssp_custom.c
@@ -20,6 +20,73 @@ extern unsigned long * visited;
 void run_sssp(int64_t root,int64_t* pred,float *dist) {
 	pred_glob=pred;
 	//user code for SSSP
+	// initialisation phase already done
+
+	// Varaibles
+	int64_t *frontier_pred; // frontière precedente
+	int current_pred;
+	int64_t *frontier_next; // frontière suivante
+	int current_next = 0;
+	float new_dist;
+	int visited[g.max_nlocalverts] = {0}; // somets visités
+
+	// to begin with
+	pred[root] = root;
+	dist[root] = 0.0;
+	visited[root] = 1;
+	// intialisation de la frontière
+	frontier_pred[0] = root;
+	current=1;
+
+	// Boucle principale
+	while(true){
+
+		// on elargi la frontière
+		for(int j=0; j<current_pred; j++){ // j -> tous les noeuds dans la frontière
+			for(int i=0; i<g.max_nlocalverts; i++){ // i -> tous les noeuds du graphe
+				// La frontièr est faite de noeuds non visités
+				if(visited[i] != 1){ 
+					if(g.column[i] == frontier_pred[j]){ // tout ceux qui sont voisins avec le noeud (de la frontière) actuel
+						frontier_next[current_next] = i; // on les rajoute à le frontière
+						current_next++;
+					}
+				}
+			}
+		}
+
+		// la condition d'arret ! (une frontière vide)
+		if(cuurent_next == 0){
+			break;
+		}
+
+		// on découvre la nouvelle frontière
+		for(int i=0; i<current_next; i++){
+			// on comapre la distance depuis le noeud actuel à celle qu'il a deja
+			new_dist= dist[root] + g.weight[root];
+			// si il n'a jamais été visité.
+			if(visited[frontier_next[i]] == 0){
+				dist[frontier_next[i]] = new_dist;
+				pred[frontier_next[i]] = root;
+				// on marque le noeud comme visité
+				visited[frontier_next[i]] = 1;
+			}else if(new_dist < dist[frontier_next[i]]){ // si il a deja été visité
+				dist[frontier_next[i]] = new_dist;
+				pred[frontier_next[i]] = root;
+			} // et sinon on fait rien
+		}
+
+		// une fois exploré la nouvelle frontière, elle n'est plus nouvelle elle devien l'ancienne
+		for(int i=0;i<current_next;i++){
+			frontier_pred[i] = frontier_next[i];
+		}
+		current_pred = current_next;
+
+		// on vide la nouvelle frontière (pour nouvelle exploration)
+		for(int i=0;i<current_next;i++){
+			frontier_next[i] = 0;
+		}
+		current_next = 0;
+	}	
 }
 
 //user provided function to prefill dist array with whatever value
